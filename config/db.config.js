6b15d21c6bbcf2f5dce9499bc0a1ee135d438ffb
halsheik <halsheik@umich.edu>
2021-10-15 19:49:21 -0400

    Fixed initial setup (prior was improper), added a connection to a MongoDB database using atlas (used database on personal account pm for more info), created basic user model and routing for testing purposes

---
+++ config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb://retrievalBot:pass@householdhomies-shard-00-00.1an6f.mongodb.net:27017,householdhomies-shard-00-01.1an6f.mongodb.net:27017,householdhomies-shard-00-02.1an6f.mongodb.net:27017/users?ssl=true&replicaSet=atlas-3u4ifg-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,14 +1,30 @@
-const http = require('http');
+const express = require("express");
+const mongoose = require("mongoose");
+const Router = require("./routes/routes");
 
-const hostname = '127.0.0.1';
-const port = 5000;
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+// parse requests of content-type - application/json
+app.use(express.json());
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// make a connection to mongoDB database
+const databaseURI = require('./config/db.config').MongoURI; // Database Config
+
+mongoose.connect(databaseURI, { useNewUrlParser: true, useUnifiedTopology: true }) 
+    .then(() => console.log('MongoDB connected...')) 
+    .catch(err => console.error("MongoDB connection error", err));
+
+app.use(Router);
+
+// routes
+// require('./routes/user.routes')(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
